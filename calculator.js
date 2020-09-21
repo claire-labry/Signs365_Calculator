67e2e555135eb9a8ae4f7e455166c84aecb7a7d2
claire-labry <clairestjlabry@gmail.com>
2020-09-21 14:45:12 -0500

    minimal functionality

--- calculator.js
+++ calculator.js
@@ -1,14 +1,42 @@
 class Calculator {
-    constructor(historyTextElement, currentTextElement){
-        this.historyTextElement = historyTextElement
-        this.currentTextElement = currentTextElement
+    constructor(previousNumTextElement, currentNumTextElement) {
+        this.previousNumTextElement = previousNumTextElement
+        this.currentNumTextElement = currentNumTextElement
+        this.clear()
+    }
+    clear(){
+        this.currentNum = ''
+        this.previousNum = ''
+        this.operation = undefined
+    }
+    delete(){}
+    appendNumber(number){
+        if(number === '.' && this.currentNum.includes('.')) 
+            return
+            this.currentNum = this.currentNum.toString() + number.toString()
+    }
+    chooseOperator(){}
+    compute(){}
+    updateDisplay(){
+        this.currentNumTextElement.innerText = this.currentNum
     }
 }
 
-const numBtn = document.querySelectorAll('[data-number]');
-const operatorBtn = document.querySelectorAll('[data-operator]');
+
+// calculator variables
+const numBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
 const equalBtn = document.querySelector('[data-equals]');
-const delBtn = document.querySelector('[data-del]')
-const clearBtn = document.querySelector('[data-clear]');
-const historyTextElement = document.querySelector('[data-history]');
-const currentTextElement = document.querySelector('[data-current]');
+const delBtn = document.querySelector('[data-delete]')
+const clearBtn = document.querySelector('[data-all-clear]');
+const previousNumTextElement = document.querySelector('[data-previous]');
+const currentNumTextElement = document.querySelector('[data-current]');
+
+const calculator = new Calculator(previousNumTextElement, currentNumTextElement)
+
+numBtns.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
