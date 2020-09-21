0f61f390c83352cc448a3238e1711ff392870115
claire-labry <clairestjlabry@gmail.com>
2020-09-21 17:35:30 -0500

    styling is finalized, javascript is now consistent with variables and quotations, and html is complted

--- calculator.js
+++ calculator.js
@@ -8,8 +8,8 @@ class Calculator {
     }
     //  function: clears the output
     clear(){
-        this.currentNum = ''
-        this.previousNum = ''
+        this.currentNum = ""
+        this.previousNum = ""
         this.operation = undefined
     }
     // function: deletes the number on the left by one
@@ -19,19 +19,19 @@ class Calculator {
     // function: allows numbers to appear next to each other
     appendNum(number){
         // ensures user doesn't type multiple periods
-        if(number === '.' && this.currentNum.includes('.')) 
+        if(number === "." && this.currentNum.includes(".")) 
             return
             this.currentNum = this.currentNum.toString() + number.toString()
     }
     // function: allows user to pick the operation on the current & previous number
     chooseOperator(operation){
-        if(this.currentNum === '') return
-        if(this.previousNum !== ''){
+        if(this.currentNum === "") return
+        if(this.previousNum !== ""){
             this.compute()
         }
         this.operation = operation
         this.previousNum = this.currentNum
-        this.currentNum = ''
+        this.currentNum = ""
     }
     // function: a switch statement that allows each operator to do the actual computation
     compute(){
@@ -40,22 +40,22 @@ class Calculator {
         const current = parseFloat(this.currentNum)
         if(isNaN(prev) || isNaN(current)) return
         switch(this.operation){
-            case '+':
+            case "+":
                 computation = prev + current
                 break
-            case '-':
+            case "-":
                 computation = prev - current
                 break
-            case '*':
+            case "*":
                 computation = prev * current
                 break
-            case '÷':
+            case "÷":
                 computation = prev / current
                 break
-            case '%':
-                computation = (prev/100) * prev
+            case "%":
+                computation = (prev/100) * current
                 break
-            // pseudocode for case '+/-':
+            // pseudocode for case "+/-"":
             //     bring in computation variable
             //     have current variable become negative 
             //     number becomes negative/positive in output
@@ -77,7 +77,7 @@ class Calculator {
         if(isNaN(integerNum)) {
             integerNumShow = ''
         } else{
-            integerNumShow = integerNum.toLocaleString('en', {maximumFractionDigits: 0})
+            integerNumShow = integerNum.toLocaleString("en", {maximumFractionDigits: 0})
         } if (decimalNum != null) {
             return `${integerNumShow}.${decimalNum}`
         } else {
@@ -91,48 +91,48 @@ class Calculator {
         if(this.operation != null){
             this.previousNumTextElement.innerText = `${this.previousNum} ${this.operation}`
         } else {
-            this.previousNumTextElement.innerText = ''
+            this.previousNumTextElement.innerText = ""
         }
     }
 }
 
 // calculator variables
-const numBtns = document.querySelectorAll('[data-number]');
-const operatorBtns = document.querySelectorAll('[data-operator]');
-const equalBtn = document.querySelector('[data-equals]');
-const delBtn = document.querySelector('[data-delete]')
-const clearBtn = document.querySelector('[data-all-clear]');
-const previousNumTextElement = document.querySelector('[data-previous]');
-const currentNumTextElement = document.querySelector('[data-current]');
+const numBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const equalBtn = document.querySelector("[data-equals]");
+const delBtn = document.querySelector("[data-delete]")
+const clearBtn = document.querySelector("[data-all-clear]");
+const previousNumTextElement = document.querySelector("[data-previous]");
+const currentNumTextElement = document.querySelector("[data-current]");
 
 const calculator = new Calculator(previousNumTextElement, currentNumTextElement)
 
 // numsBtns method: allows user to click on buttons and have them be appended/updated
 numBtns.forEach(button =>{
-    button.addEventListener('click', ()=>{
+    button.addEventListener("click", ()=>{
         calculator.appendNum(button.innerText)
         calculator.updateDisplay()
     })
 });
 // operatorBtns method: allows user to click operator of choice & and output updated
 operatorBtns.forEach(button =>{
-    button.addEventListener('click', ()=>{
+    button.addEventListener("click", ()=>{
         calculator.chooseOperator(button.innerText)
         calculator.updateDisplay()
     })
 });
 // equalBtn method: allows user to click the equal button and the compute function is fired and output is displayed
-equalBtn.addEventListener('click', button => {
+equalBtn.addEventListener("click", button => {
     calculator.compute()
     calculator.updateDisplay()
 });
 // clearBtn method: allows user to clear the output and display is blank
-clearBtn.addEventListener('click', button => {
+clearBtn.addEventListener("click", button => {
     calculator.clear()
     calculator.updateDisplay()
 });
 // delBtn method: allows user to click the del button to delete a number and update the output
-delBtn.addEventListener('click', button => {
+delBtn.addEventListener("click", button => {
     calculator.delete()
     calculator.updateDisplay()
 });
