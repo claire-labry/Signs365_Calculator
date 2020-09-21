e30fde9e992028bacf8ea5f451de15f889d6ddb3
claire-labry <clairestjlabry@gmail.com>
2020-09-21 17:00:48 -0500

    adding notes on my step by step process of building this application

--- calculator.js
+++ calculator.js
@@ -1,27 +1,29 @@
+// calculator constructor
+// this allows the previousNum and currentNum to change
 class Calculator {
     constructor(previousNumTextElement, currentNumTextElement) {
         this.previousNumTextElement = previousNumTextElement
         this.currentNumTextElement = currentNumTextElement
         this.clear()
     }
-
+    //  function: clears the output
     clear(){
         this.currentNum = ''
         this.previousNum = ''
         this.operation = undefined
     }
-
+    // function: deletes the number on the left by one
     delete(){
         this.currentNum = this.currentNum.toString().slice(0, -1)
     }
-    
+    // function: allows numbers to appear next to each other
     appendNumber(number){
         // ensures user doesn't type multiple periods
         if(number === '.' && this.currentNum.includes('.')) 
             return
             this.currentNum = this.currentNum.toString() + number.toString()
     }
-    
+    // function: allows user to pick the operation on the current & previous number
     chooseOperator(operation){
         if(this.currentNum === '') return
         if(this.previousNum !== ''){
@@ -31,7 +33,7 @@ class Calculator {
         this.previousNum = this.currentNum
         this.currentNum = ''
     }
-
+    // function: a switch statement that allows each operator to do the actual computation
     compute(){
         let computation
         const prev = parseFloat(this.previousNum)
@@ -66,7 +68,7 @@ class Calculator {
         this.operation = undefined
         this.previousNum = ''
     }
-
+    // function: allows number to be displayed in the output in string format, allows numbers to have a integer and decimal
     getDisplayNum(number){
         const stringNum = number.toString()
         const integerNum = parseFloat(stringNum.split('.')[0])
@@ -82,7 +84,7 @@ class Calculator {
             return integerNumShow
         }
     }
-
+    // function: updates the outputs display based on the chosen calculation
     updateDisplay(){
         this.currentNumTextElement.innerText = 
             this.getDisplayNum(this.currentNum)
@@ -93,7 +95,6 @@ class Calculator {
         }
     }
 }
-
 
 // calculator variables
 const numBtns = document.querySelectorAll('[data-number]');
@@ -106,30 +107,31 @@ const currentNumTextElement = document.querySelector('[data-current]');
 
 const calculator = new Calculator(previousNumTextElement, currentNumTextElement)
 
+// numsBtns method: allows user to click on buttons and have them be appended/updated
 numBtns.forEach(button =>{
     button.addEventListener('click', ()=>{
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
 });
-
+// operatorBtns method: allows user to click operator of choice & and output updated
 operatorBtns.forEach(button =>{
     button.addEventListener('click', ()=>{
         calculator.chooseOperator(button.innerText)
         calculator.updateDisplay()
     })
 });
-
+// equalBtn method: allows user to click the equal button and the compute function is fired and output is displayed
 equalBtn.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
 });
-
+// clearBtn method: allows user to clear the output and display is blank
 clearBtn.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDisplay()
 });
-
+// delBtn method: allows user to click the del button to delete a number and update the output
 delBtn.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
