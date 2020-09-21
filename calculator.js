a17941c155ba8bc17d38e894b9fe33d2fe97babd
claire-labry <clairestjlabry@gmail.com>
2020-09-21 13:07:49 -0500

    getting starter variables and calculator construtor. building functions coming up

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    
-})
+class Calculator {
+    constructor(historyTextElement, currentTextElement){
+        this.historyTextElement = historyTextElement
+        this.currentTextElement = currentTextElement
+    }
+}
+
+const numBtn = document.querySelectorAll('[data-number]');
+const operatorBtn = document.querySelectorAll('[data-operator]');
+const equalBtn = document.querySelector('[data-equals]');
+const delBtn = document.querySelector('[data-del]')
+const clearBtn = document.querySelector('[data-clear]');
+const historyTextElement = document.querySelector('[data-history]');
+const currentTextElement = document.querySelector('[data-current]');
